aa8f2320331d18d37aa0f7d3f77f661f92022782
Matthew Wright <matthewbwright@gmail.com>
2019-03-06 15:44:15 -0700

    adds internet_support function

--- cs_bot.py
+++ cs_bot.py
@@ -15,7 +15,7 @@ def cs_service_bot():
 def existing_customer():
     print(
         "What kind of support do you need? \n [1] Television Support \n [2] Internet Support \n [3] Speak to a support representative")
-    respone = input("Please enter the number corresponding to your choice: ")
+    response = input("Please enter the number corresponding to your choice: ")
     if response == "1":
         television_support()
     elif response == "2":
@@ -25,3 +25,61 @@ def existing_customer():
     else:
         print("Sorry, we didn't understand your selection.")
         existing_customer()
+
+
+def new_customer():
+    print(
+        "We're excited to have you join the DNS family, how can we help you? \n [1] Sign up for service. \n [2] Schedule a home visit. \n [3] Speak to a sales representative.")
+    response_two = input(
+        "Please enter the number corresponding to your choice: ")
+    if response_two == "1":
+        sign_up()
+    elif response_two == "2":
+        home_visit()
+    elif response_two == "3":
+        live_rep("sales")
+    else:
+        print("Sorry, we didn't understand your selection.")
+        new_customer()
+
+
+def television_support():
+    print(
+        "What is the nature of your problem? \n [1] I can't access certain channels. \n [2] My picture is blurry. \n [3] I keep losing service. \n [4] Other issue.")
+    response = input("Please enter the number corresponding to your choice: ")
+    if response == "1":
+        print("Please check the channel lists at DefinitelyNotSinister.com. If the channel you cannot access is there, please contact a live representative.")
+        did_that_help()
+    elif response == "2":
+        print("Try adjusting the antenna above your television set.")
+        did_that_help()
+    elif response == "3":
+        print(
+            "Is it raining outside? If so, wait until it is not raining and then try again.")
+        did_that_help()
+    elif response == "4":
+        live_rep("support")
+    else:
+        print("Sorry, we didn't understand your selection.")
+        television_support()
+
+
+def internet_support():
+    print(
+        "What is the nature of your problem? \n [1] I can't connect to the internet. \n [2] My connection is very slow. \n [3] I can't access certain sites. \n [4] Other issue.")
+    response = input("Please enter the number corresponding to your choice: ")
+    if response == "1":
+        print("Unplug your router, then plug it back in, then give it a good whack, like the Fonz.")
+        did_that_help()
+    elif response == "2":
+        print("Make sure that all cell phones and other people's computers are not connected to the internet, so that you can have all the bandwidth.")
+        did_that_help()
+    elif response == "3":
+        print(
+            "Move to a different region or install a VPN. Some areas block certain sites.")
+        did_that_help()
+    elif response == "4":
+        live_rep("support")
+    else:
+        print("Sorry, we didn't understand your selection.")
+        internet_support()
